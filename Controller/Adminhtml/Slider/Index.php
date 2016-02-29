7cee773a2c0fe6bb382504976c93e017d560cea0
Artis Ozolins <artis@majaslapa.lv>
2016-02-29 02:36:36 +0200

    Add menu, controller routes configuration, controller file and ACL

---
+++ Controller/Adminhtml/Slider/Index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Scandiweb_Slider
+ *
+ * @category    Scandiweb
+ * @package     Scandiweb_Slider
+ * @author      Artis Ozolins <artis@scandiweb.com>
+ * @copyright   Copyright (c) 2016 Scandiweb, Ltd (http://scandiweb.com)
+ */
+namespace Scandiweb\Slider\Controller\Adminhtml\Slider;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * Check the permission to run it
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Scandiweb_Slider::slider');
+    }
+
+    public function execute()
+    {
+    }
+}
